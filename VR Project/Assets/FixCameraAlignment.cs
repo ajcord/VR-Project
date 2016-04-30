853941d0305e0f0cdf9a4de89596b8ddd8ce0186
Stephen Salerno <stps@users.noreply.github.com>
2016-04-30 14:43:45 -0500

    fix camera positioning, and fix some of room

---
+++ VR Project/Assets/FixCameraAlignment.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixCameraAlignment : MonoBehaviour {
+
+    GameObject ts;
+
+	// Use this for initialization
+	void Start () {
+        ts = GameObject.Find("TrackingSpace");
+        Vector3 vec = ts.GetComponent<Transform>().position;
+        vec.y -= 1.4f;
+        ts.transform.position = vec;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
