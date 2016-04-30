3f33d06fa11d98ca82c876885530e700b6025cad
Stephen Salerno <stps@users.noreply.github.com>
2016-04-30 16:10:58 -0500

    more start screen

--- VR Project/Assets/Spinning.cs
+++ VR Project/Assets/Spinning.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class Spinning : MonoBehaviour {
 
     float speed = 10f;
+    GameObject cat;
 	// Use this for initialization
 	void Start () {
-	    
+        cat = GameObject.FindGameObjectWithTag("spincat");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(transform.up, speed * Time.deltaTime);
+        cat.transform.Rotate(cat.GetComponent<Transform>().up, speed * Time.deltaTime);
 	}
 }
