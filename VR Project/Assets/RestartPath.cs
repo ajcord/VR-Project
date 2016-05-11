7b3b4bbe612501af0d6931930a8accc5678ec839
Stephen Salerno <stps@users.noreply.github.com>
2016-05-10 19:36:39 -0500

    basic pathing loop

---
+++ VR Project/Assets/RestartPath.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnitySteer.Attributes;
+using UnitySteer.Tools;
+using UnitySteer.Behaviors;
+
+public class RestartPath : MonoBehaviour {
+
+    GameObject vehicle;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (GameObject.Find("Wanderer").GetComponent<AutonomousVehicle>().Speed <= 0.001f) {
+            Vector3 start = new Vector3(-0.75f, 0.05f, 0.5f);
+            GameObject.Find("Wanderer").transform.position = start;
+        }
+	}
+}
